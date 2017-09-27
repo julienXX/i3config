8c1a826d066a77965eb3d93eefe5e9918598a0ef
Julien Blanchard <julien@sideburns.eu>
2017-09-27 14:44:18 +0200

    Increase rofi font size

--- rofi.sh
+++ rofi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rofi -combi-modi "window,drun" -show combi -modi combi -lines 3 -eh 2 -width 100 -padding 810 -opacity "85" -bw 0 -bc "#2f343f" -bg "#2f343f" -fg "#f3f4f5" -bg-active "#2f343f" -fg-active "#f3f4f5" -hlbg "#2f343f" -hlbg-active "#2f343f" -hlfg "#9575cd" -hlfg-active "#9575cd" -font "System San Francisco Display Ultralight 36"
+rofi -combi-modi "window,drun" -show combi -modi combi -lines 3 -eh 2 -width 100 -padding 810 -opacity "85" -bw 0 -bc "#2f343f" -bg "#2f343f" -fg "#f3f4f5" -bg-active "#2f343f" -fg-active "#f3f4f5" -hlbg "#2f343f" -hlbg-active "#2f343f" -hlfg "#9575cd" -hlfg-active "#9575cd" -font "System San Francisco Display Ultralight 46"
